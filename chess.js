40f3260ec24609902c7769aaefc8a561c53f264f
chomp105 <outdoor10life@gmail.com>
2021-10-27 22:14:11 -0500

    added logic and polished game

--- chess.js
+++ chess.js
@@ -5,49 +5,16 @@ function not(input) {
     return 0;
 }
 
-let game = 1;
+let game, board, king, pieces, player;
+// game loop
 
-function main() { //TODO: implement advanced moves (castling, en passant, queening)
-    // game board with pieces and blank spaces
-    let board = [
-            [[ 1, 0],[ 1, 1],[ 1, 2],[ 1, 3],[ 1, 4],[ 1, 2],[ 1, 1],[ 1, 0]],
-            [[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5]],
-            [[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],
-            [[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],
-            [[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],
-            [[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],
-            [[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5]],
-            [[ 0, 0],[ 0, 1],[ 0, 2],[ 0, 3],[ 0, 4],[ 0, 2],[ 0, 1],[ 0, 0]]
-    ];
-    let king = [
-            [7, 4],
-            [0, 4]
-    ];
-    let pieces = [
-            ["\u265c", "\u265e", "\u265d", "\u265b", "\u265a", "\u265f"],
-            ["\u2656", "\u2658", "\u2657", "\u2655", "\u2654", "\u2659"]
-    ];
-    let player = 0;
-    // game loop
-
-    while(game) {
-        print_board(board, pieces);
-        console.log("player " + (player + 1) + ": ");
-        // input for the movement
-        let sx, sy, ex, ey;
-        sx = parseInt(prompt("1"));
-        sy = parseInt(prompt("2"));
-        ex = parseInt(prompt("3"));
-        ey = parseInt(prompt("4"));
-        sx--;sy--;ex--;ey--;
-        if (board[sy][sx][0] != player) {
-            continue;
-        } else if (move(board, sx, sy, ex, ey, king, player, game)) {
-            player = not(player);
-        }
+function game_eval(sx, sy, ex, ey) {
+    if (board[sy][sx][0] != player) {
+        return 0;
+    } else if (move(board, sx, sy, ex, ey, king, player, game)) {
+        player = not(player);
+        return 1;
     }
-    print_board(board, pieces);
-    console.log("Checkmate\n");
     return 0;
 }
 
@@ -86,6 +53,21 @@ function move(board, sx, sy, ex, ey, king, player, game) {
         }
         // checks for checkmate and stops game if it returns true
         if (checkmate(board, king, player)) {
+            for (let i = 0; i < pieces_arr.length; i++) {
+                if (pieces_arr[i].isGrabbed) {
+                    for (let j = 0; j < pieces_arr.length; j++) {
+                        if (i == j) {
+                            continue;
+                        }
+                        if (pieces_arr[j].x == pieces_arr[i].x && pieces_arr[j].y == pieces_arr[i].y) {
+                            pieces_arr.splice(j, 1);
+                        }
+                    }
+                }
+            }
+            clearInterval(play);
+            gui();
+            document.getElementById("reload").style.display = "block";
             game = 0;
         }
         return 1;
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let ctx = document.getElementById("gc").getContext("2d");
 
 let wr = document.getElementById("wr");
@@ -13,6 +14,8 @@ let bq = document.getElementById("bq");
 let bk = document.getElementById("bk");
 let bp = document.getElementById("bp");
 
+let prex, prey;
+
 class Piece {
     constructor(id, x, y, image) {
         this.id = id;
@@ -23,51 +26,52 @@ class Piece {
     }
 };
 
-let pieces = [
-    new Piece('r', 0, 0, br), new Piece('n', 1, 0, bn), new Piece('b', 2, 0, bb), new Piece('q', 3, 0, bq), new Piece('k', 4, 0, bk), new Piece('B', 5, 0, bb), new Piece('n', 6, 0, bn), new Piece('r', 7, 0, br),
-    new Piece('p', 0, 1, bp), new Piece('p', 1, 1, bp), new Piece('p', 2, 1, bp), new Piece('p', 3, 1, bp), new Piece('p', 4, 1, bp), new Piece('P', 5, 1, bp), new Piece('p', 6, 1, bp), new Piece('p', 7, 1, bp),
-
-    /////////////////////////////////////////////
-
-    new Piece('R', 0, 7, wr), new Piece('N', 1, 7, wn), new Piece('B', 2, 7, wb), new Piece('Q', 3, 7, wq), new Piece('K', 4, 7, wk), new Piece('B', 5, 7, wb), new Piece('N', 6, 7, wn), new Piece('R', 7, 7, wr),
-    new Piece('P', 0, 6, wp), new Piece('P', 1, 6, wp), new Piece('P', 2, 6, wp), new Piece('P', 3, 6, wp), new Piece('P', 4, 6, wp), new Piece('P', 5, 6, wp), new Piece('P', 6, 6, wp), new Piece('P', 7, 6, wp)
-];
+let pieces_arr;
 
 document.addEventListener("mousedown", (e) => {
-    for (let i = 0; i < pieces.length; i++) {
-        if (e.clientX > pieces[i].x * 100 + 10 && e.clientX < pieces[i].x * 100 + 110 && 
-            e.clientY > pieces[i].y * 100 + 10 && e.clientY < pieces[i].y * 100 + 110) {
-            pieces[i].isGrabbed = true;
-            pieces[i].x = e.clientX - 50;
-            pieces[i].y = e.clientY - 50;
+    for (let i = 0; i < pieces_arr.length; i++) {
+        if (e.clientX > pieces_arr[i].x * 100 + 10 && e.clientX < pieces_arr[i].x * 100 + 110 && 
+            e.clientY > pieces_arr[i].y * 100 + 10 && e.clientY < pieces_arr[i].y * 100 + 110) {
+            pieces_arr[i].isGrabbed = true;
+            prex = pieces_arr[i].x;
+            prey = pieces_arr[i].y;
+            pieces_arr[i].x = e.clientX - 50;
+            pieces_arr[i].y = e.clientY - 50;
         }
     }
 });
 
 document.addEventListener("mousemove", (e) => {
-    for (let i = 0; i < pieces.length; i++) {
-        if (pieces[i].isGrabbed == true) {
-            pieces[i].x = e.clientX - 50;
-            pieces[i].y = e.clientY - 50;
+    for (let i = 0; i < pieces_arr.length; i++) {
+        if (pieces_arr[i].isGrabbed == true) {
+            pieces_arr[i].x = e.clientX - 50;
+            pieces_arr[i].y = e.clientY - 50;
         }
     }
 });
 
 document.addEventListener("mouseup", (e) => {
-    for (let i = 0; i < pieces.length; i++) {
-        if (pieces[i].isGrabbed) {
-            pieces[i].isGrabbed = false;
-            pieces[i].x = e.clientX - 10 - ((e.clientX - 10) % 100);
-            pieces[i].y = e.clientY - 10 - ((e.clientY - 10) % 100);
-            pieces[i].x /= 100;
-            pieces[i].y /= 100;
-            for (let j = 0; j < pieces.length; j++) {
-                if (i == j) {
-                    continue;
+    for (let i = 0; i < pieces_arr.length; i++) {
+        if (pieces_arr[i].isGrabbed) {
+            pieces_arr[i].isGrabbed = false;
+            pieces_arr[i].x = e.clientX - 10 - ((e.clientX - 10) % 100);
+            pieces_arr[i].y = e.clientY - 10 - ((e.clientY - 10) % 100);
+            pieces_arr[i].x /= 100;
+            pieces_arr[i].y /= 100;
+            if (game_eval(prex, prey, pieces_arr[i].x, pieces_arr[i].y)) {
+                for (let j = 0; j < pieces_arr.length; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+                    if (pieces_arr[j].x == pieces_arr[i].x && pieces_arr[j].y == pieces_arr[i].y) {
+                        pieces_arr.splice(j, 1);
+                        console.log(prex, prey, pieces_arr[i].x, pieces_arr[i].y);
+                        break;
+                    }
                 }
-                if (pieces[j].x == pieces[i].x && pieces[j].y == pieces[i].y) {
-                    pieces.splice(j, 1);
-                }
+            } else {
+                pieces_arr[i].x = prex;
+                pieces_arr[i].y = prey;
             }
         }
     }
@@ -80,7 +84,7 @@ function gui() {
     for (let i = 0; i < 800; i += 100) {
         for (let j = 0; j < 800; j += 100) {
             if (count % 2 == 0) {
-                ctx.fillStyle = "#ffce9e";
+                ctx.fillStyle = "#628b96"; //"#ffce9e";
                 ctx.fillRect(i, j, 100, 100);
             }
             count++;
@@ -88,13 +92,51 @@ function gui() {
         count++;
     }
 
-    for (let i = 0; i < pieces.length; i++) {
-        if (!pieces[i].isGrabbed) {
-            ctx.drawImage(pieces[i].image, pieces[i].x * 100 + 10, pieces[i].y * 100 + 15, 80, 80);
+    for (let i = 0; i < pieces_arr.length; i++) {
+        if (!pieces_arr[i].isGrabbed) {
+            ctx.drawImage(pieces_arr[i].image, pieces_arr[i].x * 100 + 10, pieces_arr[i].y * 100 + 15, 80, 80);
         } else {
-            ctx.drawImage(pieces[i].image, pieces[i].x, pieces[i].y, 80, 80);
+            ctx.drawImage(pieces_arr[i].image, pieces_arr[i].x, pieces_arr[i].y, 80, 80);
         }
     }
 }
 
-setInterval(gui, 1000/60);
+let play = setInterval(gui, 1000/60);
+reset();
+
+function reset() {
+    pieces_arr = [
+        new Piece('r', 0, 0, br), new Piece('n', 1, 0, bn), new Piece('b', 2, 0, bb), new Piece('q', 3, 0, bq), new Piece('k', 4, 0, bk), new Piece('B', 5, 0, bb), new Piece('n', 6, 0, bn), new Piece('r', 7, 0, br),
+        new Piece('p', 0, 1, bp), new Piece('p', 1, 1, bp), new Piece('p', 2, 1, bp), new Piece('p', 3, 1, bp), new Piece('p', 4, 1, bp), new Piece('P', 5, 1, bp), new Piece('p', 6, 1, bp), new Piece('p', 7, 1, bp),
+    
+        /////////////////////////////////////////////
+    
+        new Piece('R', 0, 7, wr), new Piece('N', 1, 7, wn), new Piece('B', 2, 7, wb), new Piece('Q', 3, 7, wq), new Piece('K', 4, 7, wk), new Piece('B', 5, 7, wb), new Piece('N', 6, 7, wn), new Piece('R', 7, 7, wr),
+        new Piece('P', 0, 6, wp), new Piece('P', 1, 6, wp), new Piece('P', 2, 6, wp), new Piece('P', 3, 6, wp), new Piece('P', 4, 6, wp), new Piece('P', 5, 6, wp), new Piece('P', 6, 6, wp), new Piece('P', 7, 6, wp)
+    ];
+
+    game = 1;
+    board = [
+            [[ 1, 0],[ 1, 1],[ 1, 2],[ 1, 3],[ 1, 4],[ 1, 2],[ 1, 1],[ 1, 0]],
+            [[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5],[ 1, 5]],
+            [[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],
+            [[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],
+            [[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],
+            [[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],
+            [[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5],[ 0, 5]],
+            [[ 0, 0],[ 0, 1],[ 0, 2],[ 0, 3],[ 0, 4],[ 0, 2],[ 0, 1],[ 0, 0]]
+    ];
+    king = [
+            [7, 4],
+            [0, 4]
+    ];
+    pieces = [
+            ["\u265c", "\u265e", "\u265d", "\u265b", "\u265a", "\u265f"],
+            ["\u2656", "\u2658", "\u2657", "\u2655", "\u2654", "\u2659"]
+    ];
+    player = 0;
+
+    play = setInterval(gui, 1000/60);
+
+    document.getElementById("reload").style.display = "none";
+}
